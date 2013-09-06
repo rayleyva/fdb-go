e6ed17b2f6d043007dddc6b68a9508c8a142d28e
Ian Peters <ian.peters@foundationdb.com>
2013-09-06 12:51:57 -0400

    Remove unused error code, add new code for invalid key

--- fdb/errors.go
+++ fdb/errors.go
@@ -23,11 +23,10 @@ package fdb
 
 // SOMEDAY: these (along with others) should be coming from fdb.options?
 const (
-	errorClientInvalidOperation = 2000
+	errorKeyOutsideLegalRange = 2004
 	errorNetworkNotSetup = 2008
 
 	errorApiVersionUnset = 2200
 	errorApiVersionAlreadySet = 2201
-	errorApiVersionInvalid = 2202
 	errorApiVersionNotSupported = 2203
 )
